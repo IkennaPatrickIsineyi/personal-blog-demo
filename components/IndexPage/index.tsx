5288bfa05649a1925edbf86ce78b13939973babd
Ikenna Isineyi <ikenna.isineyi@brilloconnetz.com>
2024-02-28 11:00:19 +0100

    update added pagination to all posts section of home page

--- components/IndexPage/index.tsx
+++ components/IndexPage/index.tsx
@@ -23,7 +23,7 @@ export default function IndexPage() {
         </UiContainer>
 
         <UiContainer size="large">
-            <AllPosts posts={allPosts} />
+            <AllPosts />
         </UiContainer>
     </Box>
 }
